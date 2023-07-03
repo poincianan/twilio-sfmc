1a12af56e3a512bd30e761b182a90f807a1e195b
poincianan <17550215+poincianan@users.noreply.github.com>
2023-07-04 06:50:47 +0900

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -120,7 +120,7 @@ exports.execute = function (req, res) {
           .create({ 
              body: body,
              messagingServiceSid: messagingService,
-             to: '+818065719860'
+             to: '+'+to
            }) 
            .then(message => console.log(message.sid)) 
            .done(); 
